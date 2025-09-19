f136dbce6715ad87b776a46bf3c9cbc381972354
Nirav Kakadiya <>
2025-09-19 11:42:27 +0530

    dw

--- app/page.js
+++ app/page.js
@@ -1,58 +1,44 @@
-// app/page.js
 import { personalData } from "@/utils/data/personal-data";
-import dynamic from "next/dynamic";
 import AboutSection from "./components/homepage/about";
+import Blog from "./components/homepage/blog";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
 import Experience from "./components/homepage/experience";
+import HeroSection from "./components/homepage/hero-section";
+import Projects from "./components/homepage/projects";
+import Skills from "./components/homepage/skills";
 
-// Dynamically import client components
-const HeroSection = dynamic(
-  () => import("./components/homepage/hero-section"),
-  { ssr: false }
-);
-const Projects = dynamic(() => import("./components/homepage/projects"), {
-  ssr: false,
-});
-const Skills = dynamic(() => import("./components/homepage/skills"), {
-  ssr: false,
-});
-const Blog = dynamic(() => import("./components/homepage/blog"), {
-  ssr: false,
-});
+async function getData() {
+  const res = await fetch(
+    `https://dev.to/api/articles?username=${personalData.devUsername}`
+  );
 
-async function getBlogs() {
-  try {
-    const res = await fetch(
-      `https://dev.to/api/articles?username=${personalData.devUsername}`
-    );
-    if (!res.ok) throw new Error("Failed to fetch data");
-
-    const data = await res.json();
-    const filtered = data
-      .filter((item) => item?.cover_image)
-      .sort(() => Math.random() - 0.5);
-    return filtered;
-  } catch (error) {
-    console.error(error);
-    return [];
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
   }
+
+  const data = await res.json();
+
+  const filtered = data
+    .filter((item) => item?.cover_image)
+    .sort(() => Math.random() - 0.5);
+
+  return filtered;
 }
 
 export default async function Home() {
-  const blogs = await getBlogs();
+  const blogs = await getData();
 
   return (
     <div suppressHydrationWarning>
-      {/* Browser-dependent components are client components */}
       <HeroSection />
-      <AboutSection /> {/* Pure server component */}
-      <Experience /> {/* Pure server component */}
+      <AboutSection />
+      <Experience />
       <Skills />
       <Projects />
-      <Education /> {/* Pure server component */}
+      <Education />
       <Blog blogs={blogs} />
-      <ContactSection /> {/* Pure server component */}
+      <ContactSection />
     </div>
   );
 }
