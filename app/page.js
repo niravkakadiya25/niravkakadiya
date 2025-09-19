6d5faeb50d290b3b5e2b19eb1308861c7c84a0cf
Nirav Kakadiya <>
2025-09-19 13:01:11 +0530

    r

--- app/page.js
+++ app/page.js
@@ -1,33 +1,37 @@
+"use client";
+
 import { personalData } from "@/utils/data/personal-data";
+import dynamic from "next/dynamic";
+
+const HeroSection = dynamic(
+  () => import("./components/homepage/hero-section"),
+  { ssr: false }
+);
+const Blog = dynamic(() => import("./components/homepage/blog"), {
+  ssr: false,
+});
 import AboutSection from "./components/homepage/about";
-import Blog from "./components/homepage/blog";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
 import Experience from "./components/homepage/experience";
-import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 
-async function getData() {
+export default function HomeWrapper() {
+  return <Home />;
+}
+
+// Keep Home server component for data fetching
+async function Home() {
   const res = await fetch(
     `https://dev.to/api/articles?username=${personalData.devUsername}`
   );
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
+  if (!res.ok) throw new Error("Failed to fetch data");
 
-  const data = await res.json();
-
-  const filtered = data
+  const blogs = (await res.json())
     .filter((item) => item?.cover_image)
     .sort(() => Math.random() - 0.5);
-
-  return filtered;
-}
-
-export default async function Home() {
-  const blogs = await getData();
 
   return (
     <div suppressHydrationWarning>
